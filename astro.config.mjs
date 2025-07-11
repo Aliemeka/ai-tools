1ca9ad6658d30a9f87dcf20cca8d748e1e2531f9
Emeka Allison <emekaallison4@gmail.com>
2025-07-11 07:48:07 +0100

    Chore: Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://aitools.vercel.app",
   integrations: [mdx()],
+
   server: {
     port: 3000,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
+  adapter: vercel(),
 });
