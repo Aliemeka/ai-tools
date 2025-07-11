c53f6cea27da0e42b1be1abd1dd169042d4f267e
Emeka Allison <emekaallison4@gmail.com>
2025-07-11 07:29:24 +0100

    Fixed search and filter funtionalities

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['Instrument Serif', 'serif'],
+        'sans': ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
